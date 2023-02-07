3bd8f995bd558d3118a97256da861ac3e5896f13
noor14 <mahmedscb14@gmail.com>
2023-02-07 11:30:50 +0530

    eureka server

---
+++ src/test/java/com/noor/SpringBootPRoj01EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPRoj01EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
